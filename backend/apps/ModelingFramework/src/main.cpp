9ec48b34462b07772621034d1a123f8ede113f7e
Philipp Raithel <philipp.raithel@gmx.de>
2022-02-13 01:11:07 +0100

    added a new app project that starts a majordomo broker

---
+++ backend/apps/ModelingFramework/src/main.cpp
@@ -0,0 +1,25 @@
+#include <core/model_simulator_v2.h>
+#include <thread>
+#include "broker.h"
+#include "worker.h"
+#include "gpu/core/gpu_memory_provider_v2.h"
+
+static void RunSimWorker(const std::string& worker_address, const std::string& broker_address)
+{
+    std::unique_ptr<Service> v2 = std::make_unique<ModelSimulatorV2>();
+    Worker w(v2, worker_address, broker_address);
+    w.Start();
+}
+
+int main(int argc, char** argv)
+{
+    std::string broker_address = "tcp://0.0.0.0:5555";
+    std::string broker_bind_address = "tcp://*:5555";
+
+    std::thread t1( majordomo::BrokerActor,broker_bind_address);
+    std::thread t2(RunSimWorker, "tcp://*:5558", broker_address);
+
+    t1.join();
+    t2.join();
+	return 0;
+}
