6c521dfc74364ec1effafd854b7bfcf658f83cd1
phraith <59165291+phraith@users.noreply.github.com>
2022-03-06 21:06:52 +0100

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,2 +1,2 @@
-cmake -DCMAKE_TOOLCHAIN_FILE=/vcpkg/scripts/buildsystems/vcpkg.cmake -B ~/source/backend/build -S~/source/backend
-cmake --build ~/source/backend/build
+cmake -DCMAKE_TOOLCHAIN_FILE=/vcpkg/scripts/buildsystems/vcpkg.cmake -Bbackend/build -Sbackend
+cmake --build backend
