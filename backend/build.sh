80309da16101360cc34a491aea6d8d282fc21497
Philipp Raithel <philipp.raithel@gmx.de>
2022-03-06 19:52:14 +0100

    added build script

--- backend/build.sh
+++ backend/build.sh
@@ -1,2 +1,2 @@
-cmake -DCMAKE_TOOLCHAIN_FILE=/vcpkg/scripts/buildsystems/vcpkg.cmake -B ~/source/build -S~/source
-cmake --build ~/source/build
+cmake -DCMAKE_TOOLCHAIN_FILE=/vcpkg/scripts/buildsystems/vcpkg.cmake -B ~/source/backedn/build -S~/source/backend
+cmake --build ~/source/backend/build
