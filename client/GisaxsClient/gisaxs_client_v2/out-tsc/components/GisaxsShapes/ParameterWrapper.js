8fb44709aa1f542f05a75beac787bc8d1775efcb
phraith <59165291+phraith@users.noreply.github.com>
2022-06-04 09:50:05 +0200

    Delete client/GisaxsClient/gisaxs_client_v2/out-tsc directory

--- client/GisaxsClient/gisaxs_client_v2/out-tsc/components/GisaxsShapes/ParameterWrapper.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import TextField from '@mui/material/TextField';
-var ParameterWrapper = function (props) {
-    return (_jsx(TextField, { label: props.parameterName, type: "number", onChange: function (e) {
-            props.valueSetter(Number(e.target.value));
-        }, variant: "outlined", defaultValue: props.defaultValue }));
-};
-export default ParameterWrapper;
