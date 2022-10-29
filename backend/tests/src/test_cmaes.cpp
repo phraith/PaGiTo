04533ad8cd9967efaff41a027edb515139ba5a01
Philipp Raithel <philipp.raithel@gmx.de>
2022-10-29 12:45:02 +0200

    added test cases for cmaes calculation

---
+++ backend/tests/src/test_cmaes.cpp
@@ -0,0 +1,54 @@
+//
+// Created by phili on 23.10.2022.
+//
+
+#include "gtest/gtest.h"
+#include "cmaes/cmaes_optimizer.h"
+
+#include <vector>
+
+class CmaesTest : public ::testing::TestWithParam<int> {
+};
+
+double QuadraticFunction(const std::vector<double> &parameters) {
+    double x1 = parameters[0];
+    double x2 = parameters[1];
+    return std::pow((x1 - 3.0), 2) + std::pow((10.0 * (x2 + 2.0)), 2);
+}
+
+double EllipsoidFunction(const std::vector<double> &parameters) {
+
+    if (parameters.size() < 2) {
+        throw std::exception();
+    }
+
+    double sum = 0;
+    for (int i = 0; i < parameters.size(); ++i) {
+        sum += std::pow(1000.0, i / (parameters.size() - 1)) * std::pow(parameters.at(i), 2);
+    }
+    return sum;
+}
+
+TEST(CmaesTest, EllipsoidFunction) {
+    int dim = 5;
+    std::vector<double> initial(dim, 3);
+    CmaesOptimizer o(EllipsoidFunction, initial, 2.0, 1000);
+    std::shared_ptr<Solution> best_solution = o.Optimize();
+
+    EXPECT_NE(nullptr, best_solution);
+    for (int i = 0; i < dim; ++i) {
+        EXPECT_NEAR(0.0, best_solution->Parameters()[i], 1e-7);
+    }
+}
+
+TEST(CmaesTest, QuadraticFunction) {
+    std::vector<double> initial;
+    initial.emplace_back(0);
+    initial.emplace_back(0);
+    CmaesOptimizer o(QuadraticFunction, initial, 1.3, 1000);
+    std::shared_ptr<Solution> best_solution = o.Optimize();
+
+    EXPECT_NE(nullptr, best_solution);
+    EXPECT_FLOAT_EQ(3.0, best_solution->Parameters()[0]);
+    EXPECT_FLOAT_EQ(-2.0, best_solution->Parameters()[1]);
+}
