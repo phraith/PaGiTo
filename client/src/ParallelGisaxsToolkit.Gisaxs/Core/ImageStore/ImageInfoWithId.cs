0f3b169fe246f1c55fae575207a410a4f04c23bb
Philipp Raithel <philipp.raithel@gmx.de>
2023-01-15 18:27:33 +0100

    changed type of ImageInfoWithId to record

--- client/src/ParallelGisaxsToolkit.Gisaxs/Core/ImageStore/ImageInfoWithId.cs
+++ client/src/ParallelGisaxsToolkit.Gisaxs/Core/ImageStore/ImageInfoWithId.cs
@@ -2,14 +2,5 @@
 
 namespace ParallelGisaxsToolkit.Gisaxs.Core.ImageStore
 {
-    public class ImageInfoWithId
-    {
-        public int Id { get; }
-        public ImageInfo Info { get; }
-        public ImageInfoWithId(int id, ImageInfo info)
-        {
-            Id = id;
-            Info = info;
-        }
-    }
+    public record ImageInfoWithId(int Id, ImageInfo Info);
 }
