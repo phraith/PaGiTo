87d75df6d425391630b75e72d4e7477b0e914cc9
Philipp Raithel <philipp.raithel@gmx.de>
2023-02-25 10:19:32 +0100

    improved and simplified endpoint declarations

--- client/src/ParallelGisaxsToolkit.GisaxsClient/Endpoints/Images/GetImageEndpoint.cs
+++ client/src/ParallelGisaxsToolkit.GisaxsClient/Endpoints/Images/GetImageEndpoint.cs
@@ -1,11 +1,12 @@
-﻿using FastEndpoints;
+﻿using System.ComponentModel.DataAnnotations;
+using FastEndpoints;
 using Microsoft.AspNetCore.Authorization;
+using ParallelGisaxsToolkit.Gisaxs.Configuration;
 using ParallelGisaxsToolkit.Gisaxs.Core.ImageStore;
 using ParallelGisaxsToolkit.Gisaxs.Utility.Images;
 using ParallelGisaxsToolkit.Gisaxs.Utility.ImageTransformations;
 
 namespace ParallelGisaxsToolkit.GisaxsClient.Endpoints.Images;
-
 
 [Authorize]
 [HttpGet("/api/image/{id}/{colormap}")]
@@ -26,18 +27,18 @@ public class GetImageEndpoint : Endpoint<GetImageRequest, GetImageResponse>
             throw new InvalidOperationException("Image does not exist!");
         }
 
-        string colorizedImageAsBase64 = AppearanceModifier.ApplyColorMap(image.GreyscaleData.ToArray(), image.Info.Width,
+        string colorizedImageAsBase64 = AppearanceModifier.ApplyColorMap(image.GreyscaleData.ToArray(),
+            image.Info.Width,
             image.Info.Height, false, request.Colormap);
 
         await SendAsync(new GetImageResponse(colorizedImageAsBase64), cancellation: ct);
     }
 }
 
-public record GetImageResponse(string ImageAsBase64);
+public sealed record GetImageResponse(string ImageAsBase64);
 
-public record GetImageRequest(int Id, string Colormap)
+public sealed record GetImageRequest
 {
-    public GetImageRequest() : this(-1, string.Empty)
-    {
-    }
+    [Required] public int Id { get; init; } = -1;
+    [Required] public string Colormap { get; init; } = string.Empty;
 }
